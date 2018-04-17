5deb02e89b009ea8ec67d02fb60b2a13ce147fb9
Arne Seib <arne.seib@windy.com>
2018-04-17 09:55:43 +0200

    perf: Boost performance by loading all data into memory in an array-based quadtree.
    BREAKING CHANGE: Lookup returns now an object containing timezone info from moment-timezone.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-var find = require('./lib/find.js')
+const moment = require('moment-timezone');
+const {getTimezone, loaded} = require('./lib/find.js');
 
-module.exports = find
+//------------------------------------------------------------------------------
+// Use moment-timezone to get timezone by name
+const getTimezoneByName = (id, ts) => {
+	const zone = moment(ts).tz(id);
+	if (!zone) {
+		return null;
+	}
+	return {
+		id,
+		offset: zone._offset,
+		name: zone.format('z'),
+		designator: zone.format('Z'),
+		type: 't',
+	};
+};
+
+//------------------------------------------------------------------------------
+// Get timezone by latitude / longitude.
+const getTimezoneInfo = (lat, lng, ts /* = undefined */) => {
+	const name = getTimezone(lat, lng);
+	return name && getTimezoneByName(name, ts);
+};
+
+getTimezoneInfo.loaded = loaded;
+module.exports = getTimezoneInfo;
